248cba3cd10a03f6a70812dd91679158927b0f97
lujiesheng <2923280915@qq.com>
2021-05-27 16:59:43 +0800

    添加项目文件。

---
+++ Docker.Core.Publish.Docker.sh
@@ -0,0 +1,18 @@
+# 停止容器
+docker stop apkcontainer
+# 删除容器
+docker rm apkcontainer
+# 删除镜像
+docker rmi lenceas/apkimg
+# 切换目录
+cd /home/Docker.Core
+# 发布项目
+./Docker.Core.Publish.Linux.sh
+# 进入目录
+cd /home/Docker.Core/.PublishFiles
+# 编译镜像
+docker build -t lenceas/apkimg .
+# 生成容器
+docker run --name=apkcontainer -d -v /etc/localtime:/etc/localtime -it -p 8081:8081 lenceas/apkimg
+# 启动容器
+docker start apkcontainer
